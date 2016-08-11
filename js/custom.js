dbdc3a61c664876919efff1be42b02401f5d36ac
raodaqi <raodaqi@qq.com>
2016-08-11 09:56:31 +0800

    create

---
+++ js/custom.js
@@ -0,0 +1,37 @@
+//jQuery to collapse the navbar on scroll
+// console.log(window.screen.width);
+// IMGURL =  $(".logo-bar").attr("src");
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        // IMGURL =  $(".logo-bar").attr("src");
+        $(".white").attr("src","img/logo_black.png");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".white").attr("src","img/logo_white.png");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'jswing');
+        event.preventDefault();
+    });
+})
+
+$("#services .black-photo").on("mouseover",function(){
+    id = this.id;
+    $("#"+id+" .text2").show();
+}).on("mouseout",function(){
+    $("#"+id+" .text2").hide();
+})
+
+$(".page-scroll .button").on("mouseover",function(){
+    $(".button img").attr("src","./img/products/download_ios_on.png");
+}).on("mouseout",function(){
+    $(".button img").attr("src","./img/products/download_ios.png");
+})
